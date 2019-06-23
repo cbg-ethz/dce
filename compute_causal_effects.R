429e1fea779e424a928e14955f1b5e90f742d5a0
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-06-23 17:59:00 +0200

    Plot pathway for both normal and tumor samples

--- compute_causal_effects.R
+++ compute_causal_effects.R
@@ -13,34 +13,46 @@ df.expr.fltr <- df.expr %>%
   extract(gene, "gene.short") %>% 
   column_to_rownames("gene.short")
 
-# subset to relevant cases
-case.list <- df.classi %>%
-  filter(tissue.definition == "Solid Tissue Normal") %>%
-  pull(cases)
-node.list <- unique(c(df.graph$source, df.graph$sink))
 
-df.expr.sub <- t(df.expr.fltr[node.list, case.list])
-df.expr.sub %>% dim
-
-# prepare data
-cov.mat <- cov(df.expr.sub)
-assertthat::are_equal(dim(cov.mat)[[1]], length(node.list))
-
-g <- tidygraph::as_tbl_graph(df.graph)
-ig <- igraph::as.igraph(g)
-gn <- igraph::igraph.to.graphNEL(ig)
-# pcalg::isValidGraph(gn)
-
-# estimate causal effects
-compute_causal_effect <- function (source, sink) {
-  source.id <- match(source, node.list)
-  sink.id <- match(sink, node.list)
+# compute causal effects
+compute <- function (tissue.type) {
+  print(tissue.type)
   
-  ida.res <- pcalg::ida(source.id, sink.id, cov.mat, gn, verbose=TRUE)
-  return(ida.res)
+  # subset to relevant cases
+  case.list <- df.classi %>%
+    filter(tissue.definition == tissue.type) %>%
+    pull(cases)
+  node.list <- unique(c(df.graph$source, df.graph$sink))
+  
+  df.expr.sub <- t(df.expr.fltr[node.list, case.list])
+  print(df.expr.sub %>% dim)
+  
+  # prepare data
+  cov.mat <- cov(df.expr.sub)
+  assertthat::are_equal(dim(cov.mat)[[1]], length(node.list))
+  
+  g <- tidygraph::as_tbl_graph(df.graph)
+  ig <- igraph::as.igraph(g)
+  gn <- igraph::igraph.to.graphNEL(ig)
+  # pcalg::isValidGraph(gn)
+  
+  # estimate causal effects
+  compute_causal_effect <- function (source, sink) {
+    source.id <- match(source, node.list)
+    sink.id <- match(sink, node.list)
+    
+    ida.res <- pcalg::ida(source.id, sink.id, cov.mat, gn, verbose=FALSE)
+    return(ida.res)
+  }
+  
+  df.graph %>% 
+    rowwise() %>% 
+    mutate(causal.effect=compute_causal_effect(source, sink))
 }
 
-df.graph %>% 
-  rowwise() %>% 
-  mutate(causal.effect=compute_causal_effect(source, sink)) %>%
-  write_csv("results/graph.edgelist.csv")
+
+compute("Solid Tissue Normal") %>%
+  write_csv("results/graph.normal.edgelist.csv")
+
+compute("Primary solid Tumor") %>%
+  write_csv("results/graph.tumor.edgelist.csv")
