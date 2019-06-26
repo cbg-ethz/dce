0ca187562bb4ae470c42b5d06d0fd533a00c3a40
MartinFXP <martin.pirkl@bsse.ethz.ch>
2019-06-26 11:48:46 +0200

    added the first roxygen made helpfile and NAMESPACE

--- dce/R/main.R
+++ dce/R/main.R
@@ -5,6 +5,27 @@ library(graph)
 library(pcalg)
 library(assertthat)
 
+#' Compute the causal effect of all nodes on al other nodes of a given DAG
+#'
+#' This function takes a DAG and gene expression data as input and computes
+#' the causal effect of all nodes on all other nodes in the DAG.
+#' @param graph DAG as a graphNEL object
+#' @param df.expr genes expression profiles with rows as observations and
+#' columns as variables (nodes)
+#' @author Kim Jablonski
+#' @return vector of causal effects
+#' @export
+#' @importFrom purrr map_dfc
+#' @importFrom pcalg idaFast
+#' @importFrom assertthat are_equal
+#' @import graph tidyverse
+#' @examples
+#' dag <- matrix(c(0,0,0,1,0,0,0,1,0), 3)
+#' colnames(dag) <- rownames(dag) <- seq_len(3)
+#' dag <- as(dag, "graphNEL")
+#' d <- matrix(rnorm(100*3), 100)
+#' colnames(d) <- seq_len(3)
+#' compute_causal_effects(dag, d)
 compute_causal_effects <- function(graph, df.expr) {
   node.list <- nodes(graph)
 
@@ -16,6 +37,7 @@ compute_causal_effects <- function(graph, df.expr) {
     pcalg::idaFast(x, 1:n, cov.mat, graph)
   })
 }
+
 
 compute_differential_causal_effects <- function(
   graph.ctrl, df.expr.ctrl,
