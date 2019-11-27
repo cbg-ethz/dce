643ad5889192d5373dd18b6c8ddd6935c3aa65a6
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-11-27 17:31:40 +0100

    Use unique DAG for each benchmarking iteration

--- dce/benchmarking/differential.R
+++ dce/benchmarking/differential.R
@@ -9,6 +9,7 @@ library(dce)
 future::plan(multiprocess)
 set.seed(42)
 
+
 # helper functions
 simulate <- function(graph, noise.sd=1, sample.num=100) {
   p <- length(nodes(graph))
@@ -27,141 +28,114 @@ simulate <- function(graph, noise.sd=1, sample.num=100) {
 }
 
 
-# create graphs
-graph.num <- 10
-
+# do benchmarking
 node.num <- 30
-edge.prob <- .2
 
-negweight.range <- c(-1, -.1)
-posweight.range <- c(.1, 1)
+parameter.list <- c(50, 100, 1000)
 
-graph.list <- purrr::imap(1:graph.num, function (x, i) {
+df.bench <- furrr::future_pmap_dfr(parameter.list, function(parameter) {
+  # create graphs
+  edge.prob <- runif(1, 0, 1)
+
+  negweight.range <- c(-1, 0)
+  posweight.range <- c(0, 1)
+
   wt.graph <- create_random_DAG(node.num, edge.prob, negweight.range, posweight.range)
   mt.graph <- resample_edge_weights(wt.graph, negweight.range, posweight.range)
 
-  list(graph.idx=i, wt.graph=wt.graph, mt.graph=mt.graph)
-})
 
-graph.list %>% head(1)
+  # generate data
+  wt.X <- simulate(wt.graph, sample.num=parameter)
+  mt.X <- simulate(mt.graph, sample.num=parameter)
 
 
-# compute graph features
-graph.features <- purrr::map_df(graph.list, function (graph.pair) {
-  tmp <- as(graph.pair$wt.graph, "matrix")
-  tmp[which(tmp != 0)] <- 1
+  # run models
+  ground.truth <- list(dce=trueEffects(wt.graph) - trueEffects(mt.graph))
 
-  data.frame(
-    graph.idx=graph.pair$graph.idx,
-    density=sum(tmp) / length(tmp)
+  time.tmp <- Sys.time()
+  res.cor <- list(dce=cor(wt.X) - cor(mt.X))
+  time.cor <- Sys.time() - time.tmp
+
+  time.tmp <- Sys.time()
+  res.basic <- compute_differential_causal_effects(
+    wt.graph, wt.X,
+    mt.graph, mt.X,
+    method="basic"
   )
-}) %>%
-  write_csv("graph_features.csv")
+  time.basic <- Sys.time() - time.tmp
 
-graph.features %>% head(1)
+  time.tmp <- Sys.time()
+  res.basic.bootstrap <- compute_differential_causal_effects(
+    wt.graph, wt.X,
+    mt.graph, mt.X,
+    method="basic", bootstrap=TRUE
+  )
+  time.basic.bootstrap <- Sys.time() - time.tmp
 
+  time.tmp <- Sys.time()
+  res.full <- compute_differential_causal_effects(
+    wt.graph, wt.X,
+    mt.graph, mt.X,
+    method="full"
+  )
+  time.full <- Sys.time() - time.tmp
 
-# do benchmarking
-parameter.list <- c(50, 100, 1000)
-repetition.num <- 20
+  time.tmp <- Sys.time()
+  res.full.bootstrap <- compute_differential_causal_effects(
+    wt.graph, wt.X,
+    mt.graph, mt.X,
+    method="full", bootstrap=TRUE
+  )
+  time.full.bootstrap <- Sys.time() - time.tmp
 
-input.list <- purrr::cross_df(list(graph.pair=graph.list, parameter=parameter.list))
+  time.tmp <- Sys.time()
+  tmp <- as.matrix(ground.truth$dce)
+  tmp[which(as.matrix(ground.truth$dce) != 0)] = (
+    runif(sum(as.matrix(ground.truth$dce) != 0), negweight.range[1], posweight.range[2]) -
+    runif(sum(as.matrix(ground.truth$dce) != 0), negweight.range[1], posweight.range[2])
+  )
+  res.rand <- list(dce=tmp)
+  time.rand <- Sys.time() - time.tmp
 
-df.bench <- furrr::future_pmap_dfr(input.list, function(graph.pair, parameter) {
-  purrr::map_df(seq_len(repetition.num), function(x) {
-    # generate data
-    wt.X <- simulate(graph.pair$wt.graph, sample.num=parameter)
-    mt.X <- simulate(graph.pair$mt.graph, sample.num=parameter)
-
-
-    # run models
-    ground.truth <- list(dce=trueEffects(graph.pair$wt.graph) - trueEffects(graph.pair$mt.graph))
-
-    time.tmp <- Sys.time()
-    res.cor <- list(dce=cor(wt.X) - cor(mt.X))
-    time.cor <- Sys.time() - time.tmp
-
-    time.tmp <- Sys.time()
-    res.basic <- compute_differential_causal_effects(
-      graph.pair$wt.graph, wt.X,
-      graph.pair$mt.graph, mt.X,
-      method="basic"
-    )
-    time.basic <- Sys.time() - time.tmp
-
-    time.tmp <- Sys.time()
-    res.basic.bootstrap <- compute_differential_causal_effects(
-      graph.pair$wt.graph, wt.X,
-      graph.pair$mt.graph, mt.X,
-      method="basic", bootstrap=TRUE
-    )
-    time.basic.bootstrap <- Sys.time() - time.tmp
-
-    time.tmp <- Sys.time()
-    res.full <- compute_differential_causal_effects(
-      graph.pair$wt.graph, wt.X,
-      graph.pair$mt.graph, mt.X,
-      method="full"
-    )
-    time.full <- Sys.time() - time.tmp
-
-    time.tmp <- Sys.time()
-    res.full.bootstrap <- compute_differential_causal_effects(
-      graph.pair$wt.graph, wt.X,
-      graph.pair$mt.graph, mt.X,
-      method="full", bootstrap=TRUE
-    )
-    time.full.bootstrap <- Sys.time() - time.tmp
-
-    time.tmp <- Sys.time()
-    tmp <- as.matrix(ground.truth$dce)
-    tmp[which(as.matrix(ground.truth$dce) != 0)] = (
-      runif(sum(as.matrix(ground.truth$dce) != 0), negweight.range[1], posweight.range[2]) -
-      runif(sum(as.matrix(ground.truth$dce) != 0), negweight.range[1], posweight.range[2])
-    )
-    res.rand <- list(dce=tmp)
-    time.rand <- Sys.time() - time.tmp
-
-    df.res <- data.frame(
-      truth=as.vector(ground.truth$dce),
-      cor=as.vector(res.cor$dce),
-      basic=as.vector(res.basic$dce),
-      basic.bootstrap=as.vector(res.basic.bootstrap$dce),
-      full=as.vector(res.full$dce),
-      full.bootstrap=as.vector(res.full.bootstrap$dce),
-      rand=as.vector(res.rand$dce)
-    )
+  df.res <- data.frame(
+    truth=as.vector(ground.truth$dce),
+    cor=as.vector(res.cor$dce),
+    basic=as.vector(res.basic$dce),
+    basic.bootstrap=as.vector(res.basic.bootstrap$dce),
+    full=as.vector(res.full$dce),
+    full.bootstrap=as.vector(res.full.bootstrap$dce),
+    rand=as.vector(res.rand$dce)
+  )
 
 
-    # compute performance
-    df.perf <- as.data.frame(
-      cor(df.res, method="spearman") #  %>% dplyr::filter(truth != 0)
-    )
-
-    # return result
-    df.perf %>%
-      rownames_to_column() %>%
-      dplyr::filter(rowname == "truth") %>%
-      select(-rowname, -truth) %>%
-      mutate(type="performance") %>%
-      bind_rows(
-        data.frame(
-          cor=time.cor,
-          basic=time.basic,
-          basic.bootstrap=time.basic.bootstrap,
-          full=time.full,
-          full.bootstrap=time.full.bootstrap,
-          rand=time.rand
-        ) %>%
-          mutate(type="runtime")
+  # return performance computation
+  data.frame() %>%
+    bind_rows(
+      as.data.frame(
+        cor(df.res, method="spearman")
       ) %>%
-      mutate(parameter=parameter, graph.idx=graph.pair$graph.idx)
-  })
+        rownames_to_column() %>%
+        dplyr::filter(rowname == "truth") %>%
+        select(-rowname, -truth) %>%
+        mutate(type="correlation"),
+
+      data.frame(
+        cor=time.cor,
+        basic=time.basic,
+        basic.bootstrap=time.basic.bootstrap,
+        full=time.full,
+        full.bootstrap=time.full.bootstrap,
+        rand=time.rand
+      ) %>%
+        mutate(type="runtime"),
+
+    ) %>%
+    mutate(parameter=parameter)
 }, .progress=TRUE) %>%
   write_csv("benchmark_results.csv")
 
 
-df.bench$parameter <- fct_inseq(as_factor(df.bench$parameter))
+df.bench$parameter %<>% as.factor %>% fct_inseq
 df.bench %>%
   head
 
