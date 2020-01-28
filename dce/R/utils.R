e1be573cac04b1ad5c5722187b33d8b1e8398630
mpirkl <martin.pirkl@bsse.ethz.ch>
2020-01-28 15:07:23 +0100

    initialize p-value matrix with NAs and not zeros

--- dce/R/utils.R
+++ dce/R/utils.R
@@ -247,7 +247,7 @@ fulllin <- function(g1, d1, g2, d2, conf = TRUE,
     df <- as.data.frame(cbind(df, N = c(rep(1, nrow(d1)), rep(0, nrow(d2)))))
     n <- length(nodes(g1))
     dce <- mat1*0
-    dce.p <- mat1*0
+    dce.p <- mat1*NA
     glmfun <- function(...) {
         fun <- "glm2"
         linkfun <- "log"
