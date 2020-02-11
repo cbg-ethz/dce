6cd164fae5e1c6d1e6bc1546f66d4ab96dde70af
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-02-11 10:52:20 +0100

    Update dce tests

--- dce/tests/testthat/test-dce.R
+++ dce/tests/testthat/test-dce.R
@@ -1,12 +1,28 @@
-test_that("something works", {
+test_that("positive beta can be recovered", {
   set.seed(42)
 
-  g <- as(matrix(c(0,0,1,0), 2, 2), "graphNEL")
-  d <- simulate_data(g)
+  graph.wt <- as(matrix(c(0, 0, 1e-42, 0), 2, 2), "graphNEL")
+  X.wt <- simulate_data(graph.wt)
 
-  g2 <- as(matrix(c(0,0,2.3,0), 2, 2), "graphNEL")
-  d2 <- simulate_data(g2)
+  graph.mt <- as(matrix(c(0, 0, 1.5, 0), 2, 2), "graphNEL")
+  X.mt <- simulate_data(graph.mt)
 
-  res <- compute_differential_causal_effects(g, d, g2, d2)
-  expect_equal(as.vector(res$dce), c(0, 0, -1.3, 0), tolerance=0.1)
+  res <- compute_differential_causal_effects(graph.wt, X.wt, graph.mt, X.mt)
+
+  expect_equal(as.vector(res$dce), c(0, 0, 1.5, 0), tolerance=0.1)
+})
+
+
+test_that("negative beta can be recovered", {
+  set.seed(42)
+
+  graph.wt <- as(matrix(c(0, 0, 1e-42, 0), 2, 2), "graphNEL")
+  X.wt <- simulate_data(graph.wt)
+
+  graph.mt <- igraph::igraph.to.graphNEL(igraph::graph_from_adjacency_matrix(matrix(c(0, 0, -1.5, 0), 2, 2)))
+  X.mt <- simulate_data(graph.mt)
+
+  res <- compute_differential_causal_effects(graph.wt, X.wt, graph.mt, X.mt)
+
+  expect_equal(as.vector(res$dce), c(0, 0, -1.5, 0), tolerance=0.1)
 })
