4ca10749f393b26966c1eba8d51d420ef8ccc202
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-02-11 18:11:46 +0100

    Make pathway plots comparable by normalizing edge color scale

--- tcga_pipeline/scripts/visualize.R
+++ tcga_pipeline/scripts/visualize.R
@@ -10,7 +10,10 @@ df.genes <- read_csv(snakemake@input$geneid_fname)
 geneid.map <- setNames(as.character(df.genes$SYMBOL), df.genes$ENSEMBL)
 
 # plot
-p.list <- lapply(res, plot, nodename.map=geneid.map)
+dce.abs.max <- max(sapply(res, function(x) { max(abs(x$dce)) }))
+custom.limits <- c(-dce.abs.max, dce.abs.max)
+
+p.list <- lapply(res, plot, nodename.map=geneid.map, edge.colorscale.limits=custom.limits)
 
 p <- cowplot::plot_grid(
   plotlist=p.list, labels=names(p.list),
