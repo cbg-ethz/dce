5d5db0af9cca5e067814b9ae2261b52997dca9a2
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-02-19 09:29:43 +0100

    Allow 'simulate_data(..)' to take igraph, graphNEL, and matrix input networks

--- dce/R/data_simulation.R
+++ dce/R/data_simulation.R
@@ -14,50 +14,103 @@
 #' @examples
 #' dag <- create_random_DAG(30, 0.2)
 #' X <- simulate_data(dag)
-simulate_data <- function(
-  dag, n = 100,
-  dist.mean = 1000, dist.dispersion = 100,
-  link.log.base=exp(1)
-) {
-  p <- length(nodes(dag))
-  adj.mat <- igraph::as_adjacency_matrix(
-    igraph::igraph.from.graphNEL(dag),
-    attr=if (numEdges(dag) > 0) "weight" else NULL
-  ) %>%
-    as.matrix %>%
-    t
+setGeneric(
+    "simulate_data",
+    function(
+        graph, n = 100,
+        dist.mean = 1000, dist.dispersion = 100,
+        link.log.base = exp(1)
+    ) {
+        standardGeneric("simulate_data")
+    },
+    package = "dce"
+)
 
-  # sanity checks
-  stopifnot(is(dag, "graph"), p >= 2)
 
-  nonzero.idx <- which(adj.mat != 0, arr.ind = TRUE)
-  if (nrow(nonzero.idx) > 0) {
-    if (any(nonzero.idx[, 1] - nonzero.idx[, 2] < 0) || any(diag(adj.mat) != 0)) {
-      stop("Input DAG must be topologically ordered!")
+setOldClass("igraph")
+setMethod(
+    "simulate_data",
+    signature = signature(graph = "igraph"),
+    function(
+        graph, n = 100,
+        dist.mean = 1000, dist.dispersion = 100,
+        link.log.base = exp(1)
+    ) {
+        simulate_data(
+            as(igraph::as_adjacency_matrix(
+                graph,
+                attr = if ("weight" %in% igraph::edge_attr_names(graph)) "weight" else NULL
+            ), "matrix"),
+            n, dist.mean, dist.dispersion,
+            link.log.base
+        )
     }
-  }
+)
 
-  # setup data
-  X <- matrix(rnbinom(n * p, size=dist.dispersion, mu=dist.mean), nrow=n, ncol=p)
-  colnames(X) <- nodes(dag)
 
-  # simulate data
-  link <- make.log.link(link.log.base)
-
-  for (j in seq(2, p)) {
-    ij <- seq_len(j - 1)
-    betas <- adj.mat[j, ij]
-
-    if (any(betas != 0)) {
-      # current node has parents
-      if (link.log.base == 0) {
-        betasX <- X[, ij, drop = FALSE] %*% betas
-        mu <- dist.mean + betasX - min(betasX)
-      } else {
-        mu <- link$linkinv(log(dist.mean, link.log.base) + scale(X[, ij, drop = FALSE], scale=FALSE) %*% betas)
-      }
-      X[, j] <- rnbinom(n, size=dist.dispersion, mu=mu)
+setMethod(
+    "simulate_data",
+    signature = signature(graph = "graphNEL"),
+    function(
+        graph, n = 100,
+        dist.mean = 1000, dist.dispersion = 100,
+        link.log.base = exp(1)
+    ) {
+        simulate_data(
+            as(graph, "matrix"),
+            n, dist.mean, dist.dispersion,
+            link.log.base
+        )
     }
-  }
-  return(X)
-}
+)
+
+
+setMethod(
+    "simulate_data",
+    signature = signature(graph = "matrix"),
+    function(
+        graph, n = 100,
+        dist.mean = 1000, dist.dispersion = 100,
+        link.log.base = exp(1)
+    ) {
+        p <- dim(graph)[[1]]
+
+        # sanity checks
+        stopifnot(p >= 2)
+
+        nonzero.idx <- which(graph != 0, arr.ind = TRUE)
+        if (
+            (nrow(nonzero.idx) > 0) &&
+            (
+                any(nonzero.idx[, 2] - nonzero.idx[, 1] < 0) ||
+                any(diag(graph) != 0)
+            )
+        ) {
+            stop("Input DAG must be topologically ordered!")
+        }
+
+        # setup data
+        X <- matrix(rnbinom(n * p, size=dist.dispersion, mu=dist.mean), nrow=n, ncol=p)
+        colnames(X) <- colnames(graph)
+
+        # simulate data
+        link <- make.log.link(link.log.base)
+
+        for (j in seq(2, p)) {
+          ij <- seq_len(j - 1)
+          betas <- graph[ij, j]
+
+          if (any(betas != 0)) {
+            # current node has parents
+            if (link.log.base == 0) {
+              betasX <- X[, ij, drop = FALSE] %*% betas
+              mu <- dist.mean + betasX - min(betasX)
+            } else {
+              mu <- link$linkinv(log(dist.mean, link.log.base) + scale(X[, ij, drop = FALSE], scale=FALSE) %*% betas)
+            }
+            X[, j] <- rnbinom(n, size=dist.dispersion, mu=mu)
+          }
+        }
+        return(X)
+    }
+)
