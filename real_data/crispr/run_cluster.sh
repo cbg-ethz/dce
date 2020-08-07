ab113eb600faaf57d55b42ef4d7bf1382731d048
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-08-07 16:15:40 +0200

    Use 'lsf.yaml' to allow longer queue times

--- real_data/crispr/run_cluster.sh
+++ real_data/crispr/run_cluster.sh
@@ -1,3 +1,7 @@
+workdir="$(grep ^workdir config.yaml | cut -d ' ' -f 2 | tr -d "'")" # oh no, very bad
+mkdir -p "./$workdir"
+cp -v "lsf.yaml" "./$workdir"
+
 bsub \
   -N \
   -R 'rusage[mem=2000]' \
