dfcc96cb5f8dd25b4ab41e21875dcefdf30cd8da
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-08-24 17:29:00 +0200

    *cough*

--- real_data/crispr/scripts/compute_dces.R
+++ real_data/crispr/scripts/compute_dces.R
@@ -37,6 +37,6 @@ ggsave(file.path(out.dir, glue::glue("network_{appendix}.pdf")), width=20, heigh
 
 res %>%
   as.data.frame %>%
-  arrange(desc(abs(dce.pvalue))) %>%
+  arrange(dce.pvalue) %>%
   drop_na %>%
   write_csv(file.path(out.dir, glue::glue("dce_list_{appendix}.csv")))
