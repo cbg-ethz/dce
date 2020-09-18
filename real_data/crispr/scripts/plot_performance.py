d8073d68cd95d09ae606702a6344182a37d4dbe9
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-09-18 10:26:26 +0200

    Add rule to plot performance measure results

---
+++ real_data/crispr/scripts/plot_performance.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def main(dname, out_dir):
+    df = pd.read_csv(dname / 'measures.csv').set_index(['study', 'treatment', 'perturbed_gene', 'pathway'])
+
+    # general overview
+    plt.figure(figsize = (8, 6))
+
+    sns.boxplot(data=pd.melt(df), x='variable', y='value')
+
+    plt.xlabel('Performance measure')
+    plt.ylabel('Score')
+
+    plt.tight_layout()
+    plt.savefig(out_dir / 'overview_boxplot.pdf')
+
+
+if __name__ == '__main__':
+    main(Path(snakemake.input.dname), Path(snakemake.output.out_dir))
