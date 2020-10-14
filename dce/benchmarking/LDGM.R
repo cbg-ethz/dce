5ffcda41c45ac2f8d1f2783d1ab59b8b3b1f1a7e
mpirkl <martin.pirkl@bsse.ethz.ch>
2020-10-14 11:28:56 +0200

    switched matrices for ldgm difference

--- dce/benchmarking/LDGM.R
+++ dce/benchmarking/LDGM.R
@@ -9,12 +9,9 @@ LDGM <- function(wt.X,mt.X) {
     write.table(mt.LC,file=mt.f,row.names=FALSE,col.names=FALSE,sep=",")
 
     theta.f <- paste0("Theta.",runif(1),".csv")
-    system(paste0("matlab -batch \"Sigma1 = readmatrix('",wt.f,"'); Sigma2 = readmatrix('",mt.f,"'); lambda = 0.26924; cd('LDGM'); Theta = differential_graph(Sigma1,Sigma2,lambda); writematrix(Theta,'",theta.f,"'); exit\""))
+    system(paste0("matlab -batch \"Sigma1 = readmatrix('",mt.f,"'); Sigma2 = readmatrix('",wt.f,"'); lambda = 0.26924; cd('LDGM'); Theta = differential_graph(Sigma1,Sigma2,lambda); writematrix(Theta,'",theta.f,"'); exit\""))
     Theta <- as(read.csv(paste0("LDGM/",theta.f),header=FALSE),"matrix")
 
-    ## Theta.z <- solve(Theta)
-    ## Theta.z <- 0.5*(1+Theta.z)/(1-Theta.z)
-    
     system(paste0("rm ", wt.f, " ", mt.f, " LDGM/", theta.f))
     return(list(dce=Theta,dce_pvalue=abs(Theta)))
 }
