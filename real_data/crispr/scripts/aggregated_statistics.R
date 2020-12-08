fe01c78e5afa8f49c2a93b7abeaab0e9cc6641d5
kpj <kim.philipp.jablonski@gmail.com>
2020-12-08 09:52:56 +0100

    Create aggregated distance plot

--- real_data/crispr/scripts/aggregated_statistics.R
+++ real_data/crispr/scripts/aggregated_statistics.R
@@ -10,3 +10,21 @@ dir.create(out_dir, recursive = TRUE)
 
 # read data
 df_all <- read_csv(fname)
+
+df_all %>%
+  head
+
+
+# network distance plot
+df_all %>%
+  filter(!is.infinite(distance)) %>%
+  drop_na() %>%
+ggplot(aes(x = as.factor(distance), y = abs(dce))) +
+  geom_boxplot() +
+  scale_y_log10() +
+  xlab("Graph distance: perturbed-gene to DCE-edge") +
+  ylab("DCE (only >0.5)") +
+  ylim(0.5, max(abs(df_all$dce))) +
+  facet_wrap(~ study) +
+  theme_minimal()
+ggsave(file.path(out_dir, glue::glue("network_distances.pdf")), width = 8, height = 6)
