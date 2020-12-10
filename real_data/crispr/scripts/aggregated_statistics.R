f56e03c0293c6d393a543985ebc3ad5d891ad9a9
kpj <kim.philipp.jablonski@gmail.com>
2020-12-10 14:30:58 +0100

    Investigate pathway coverage

--- real_data/crispr/scripts/aggregated_statistics.R
+++ real_data/crispr/scripts/aggregated_statistics.R
@@ -15,6 +15,25 @@ df_all %>%
   head
 
 
+# pathway coverage investigation
+df_cov <- df_all %>%
+  group_by(study, treatment, perturbed_gene, pathway) %>%
+  summarize(dce_na_fraction = sum(is.na(dce)) / n()) %>%
+  arrange(dce_na_fraction)
+
+df_cov %>%
+  head
+df_cov %>%
+  tail
+
+df_cov %>%
+ggplot(aes(x = dce_na_fraction)) +
+  geom_histogram() +
+  facet_wrap(~ study) +
+  theme_minimal()
+ggsave(file.path(out_dir, glue::glue("network_coverage.pdf")), width = 8, height = 6)
+
+
 # network distance plot
 df_all %>%
   filter(!is.infinite(distance)) %>%
