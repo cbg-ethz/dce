6a41e6265db74fd9a29cbd3814b3000601e045f5
kpj <kim.philipp.jablonski@gmail.com>
2020-12-15 16:23:34 +0100

    Refactor pathway statistic computation

--- real_data/crispr/scripts/pathway_statistics.R
+++ real_data/crispr/scripts/pathway_statistics.R
@@ -2,70 +2,47 @@ library(tidyverse)
 
 
 # parameters
-fname.expr.wt <- snakemake@input$count_wt_file
-graph.files <- snakemake@input$graph_files
+fname_expr_wt <- snakemake@input$count_wt_file
+graph_files <- snakemake@input$graph_files
 
-perturbed.genes <- snakemake@params$perturbed_genes
+perturbed_genes <- snakemake@params$perturbed_genes
 
-out.dir <- snakemake@output$out_dir
-dir.create(out.dir, recursive = TRUE)
+out_dir <- snakemake@output$out_dir
+dir.create(out_dir, recursive = TRUE)
 
 
 # handle multiple knockouts
-perturbed.genes %<>%
+perturbed_genes %<>%
   purrr::map(~ strsplit(., ",")[[1]]) %>%
   unlist %>%
   unique
 
 
-# degree distribution
-df.degree <- purrr::map_dfr(graph.files, function(fname) {
+# read auxiliary data
+df_expr_wt <- read_csv(fname_expr_wt) %>% column_to_rownames("X1") %>% as.matrix
+
+
+# compute per-pathway statistics
+df_stats <- purrr::map_dfr(graph_files, function(fname) {
+  # read pathway
   graph <- igraph::graph.data.frame(read_csv(
     fname, col_types = cols(
       source = col_character(),
       sink = col_character()
     )
   ))
-  pathway.name <- strsplit(basename(fname), "\\.")[[1]][[1]]
+  pathway_name <- strsplit(basename(fname), "\\.")[[1]][[1]]
 
-  genes <- intersect(igraph::vertex_attr(graph, "name"), perturbed.genes)
-  if (length(genes) == 0) {
-    return(data.frame())
-  }
+  # pathway graph properties
+  mean_degree <- mean(igraph::degree(graph, mode = "all"))
+  mean_degree_out <- mean(igraph::degree(graph, mode = "out"))
+  mean_degree_in <- mean(igraph::degree(graph, mode = "in"))
 
-  degree.all <- igraph::degree(graph, mode = "all")[genes]
-  degree.out <- igraph::degree(graph, mode = "out")[genes]
-  degree.in <- igraph::degree(graph, mode = "in")[genes]
-
-  data.frame(pathway = pathway.name, genes = genes, degree.all = degree.all, degree.out = degree.out, degree.in = degree.in)
-}) %>%
-  arrange(desc(degree.all)) %>%
-  write_csv(file.path(out.dir, "perturbed_gene_pathway_degrees.csv"))
-
-df.degree %>% arrange(desc(degree.in)) %>% head
-
-ggplot(df.degree, aes(x = degree.in)) +
-  geom_histogram() +
-  theme_minimal()
-ggsave(file.path(out.dir, "indegree_distribution.pdf"))
-
-
-# count distribution
-df.expr.wt <- read_csv(fname.expr.wt) %>% column_to_rownames("X1") %>% as.matrix
-
-df.counts <- purrr::map_dfr(graph.files, function(fname) {
-  graph <- igraph::graph.data.frame(read_csv(
-    fname, col_types = cols(
-      source = col_character(),
-      sink = col_character()
-    )
-  ))
-  pathway.name <- strsplit(basename(fname), "\\.")[[1]][[1]]
-
+  # determine expression coverage along pathway
   pathway_nodes <- igraph::vertex_attr(graph, "name")
-  common_genes <- intersect(pathway_nodes, rownames(df.expr.wt))
+  common_genes <- intersect(pathway_nodes, rownames(df_expr_wt))
 
-  df_sub <- df.expr.wt[common_genes, ]
+  df_sub <- df_expr_wt[common_genes, ]
   if (is.null(dim(df_sub))) {
     # only a single gene was selected
     stopifnot(length(common_genes) == 1)
@@ -75,24 +52,71 @@ df.counts <- purrr::map_dfr(graph.files, function(fname) {
     gene_medians <- matrixStats::rowMedians(df_sub)
   }
 
-  median_count <- median(gene_medians)
-  good_counts <- sum(gene_medians > 1)
-  good_count_fraction <- good_counts / length(gene_medians)
+  meanofmedian_gene_expression <- mean(gene_medians)
 
   data.frame(
-    pathway = pathway.name,
-    pathway_size = length(pathway_nodes),
+    pathway = pathway_name,
+
+    pathway_node_size = igraph::vcount(graph),
+    pathway_edge_size = igraph::ecount(graph),
+    pathway_density = igraph::ecount(graph) / (igraph::vcount(graph) * (igraph::vcount(graph) - 1)),
+    mean_degree = mean_degree,
+    mean_degree_out = mean_degree_out,
+    mean_degree_in = mean_degree_in,
+
     pathway_countmatrix_overlap = length(common_genes),
-    median_count = median_count,
-    good_counts = good_counts,
-    good_count_fraction = good_count_fraction
+
+    meanofmedian_gene_expression = meanofmedian_gene_expression
   )
 }) %>%
-  arrange(desc(good_count_fraction)) %>%
-  write_csv(file.path(out.dir, "pathway_count_statistics.csv"))
+  write_csv(file.path(out_dir, "pathway_statistics.csv"))
 
-ggplot(df.counts, aes(x = good_count_fraction)) +
+
+# degree distribution of perturbed genes
+df_degree <- purrr::map_dfr(graph_files, function(fname) {
+  graph <- igraph::graph.data.frame(read_csv(
+    fname, col_types = cols(
+      source = col_character(),
+      sink = col_character()
+    )
+  ))
+  pathway_name <- strsplit(basename(fname), "\\.")[[1]][[1]]
+
+  genes <- intersect(igraph::vertex_attr(graph, "name"), perturbed_genes)
+  if (length(genes) == 0) {
+    return(data.frame())
+  }
+
+  degree_all <- igraph::degree(graph, mode = "all")[genes]
+  degree_out <- igraph::degree(graph, mode = "out")[genes]
+  degree_in <- igraph::degree(graph, mode = "in")[genes]
+
+  data.frame(
+    pathway = pathway_name,
+    genes = genes,
+
+    degree_all = degree_all,
+    degree_out = degree_out,
+    degree_in = degree_in
+  )
+}) %>%
+  arrange(desc(degree_all)) %>%
+  write_csv(file.path(out_dir, "perturbed_gene_pathway_degrees.csv"))
+
+df_degree %>% arrange(desc(degree_in)) %>% head
+
+ggplot(df_degree, aes(x = degree_in)) +
   geom_histogram() +
-  xlab("Fraction of pathway nodes with median counts > 1") +
   theme_minimal()
-ggsave(file.path(out.dir, "good_count_distribution.pdf"))
+ggsave(file.path(out_dir, "indegree_distribution.pdf"))
+
+
+# pathway statistic plots
+df_stats %>%
+  select_if(is.numeric) %>%
+  gather %>%
+ggplot(aes(x = value)) +
+  geom_histogram() +
+  facet_wrap(~ key, scales = "free") +
+  theme_minimal()
+ggsave(file.path(out_dir, "pathway_statistics.pdf"))
