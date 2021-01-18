0dd1f1ca662d095348db4d90a423d01be776c51f
kpj <kim.philipp.jablonski@gmail.com>
2021-01-18 12:13:55 +0100

    Stratify performance plot by treatment

--- real_data/crispr/scripts/plot_performance.py
+++ real_data/crispr/scripts/plot_performance.py
@@ -24,13 +24,13 @@ def main(dname, out_dir):
     plt.savefig(out_dir / 'overview_boxplot.pdf')
 
     # compare methods
-    df_sub = pd.melt(df.reset_index(level='method')[['method', 'roc_auc']], id_vars=['method'])
+    df_sub = pd.melt(df.reset_index(level=['method', 'treatment'])[['method', 'treatment', 'roc_auc']], id_vars=['method', 'treatment'])
 
     plt.figure(figsize=(8, 6))
 
     sns.boxplot(
         data=df_sub,
-        x='method', y='value')
+        x='method', y='value', hue='treatment')
 
     plt.xlabel('Method')
     plt.ylabel('ROC-AUC')
