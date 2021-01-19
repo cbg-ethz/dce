7c73b62f91e709a6ec80e7e801698e02e899b319
kpj <kim.philipp.jablonski@gmail.com>
2021-01-19 18:05:18 +0100

    Add pathway statistics as lazy data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Biological pathway information.
+#'
+#' A dataset containing pathway statistics.
+#'
+#' @format A data frame with pathway statistics
+#' \describe{
+#'   \item{database}{Pathway database}
+#'   ...
+#' }
+"df_pathway_statistics"
