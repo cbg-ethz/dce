dfcbb3ce56f838a4b15e02774d2b21a9b50d4621
kpj <kim.philipp.jablonski@gmail.com>
2021-02-27 15:49:50 +0100

    Remove workdir shenanigans from execution script

--- inst/scripts/crispr_benchmark/run_cluster.sh
+++ inst/scripts/crispr_benchmark/run_cluster.sh
@@ -1,7 +1,3 @@
-workdir="$(grep ^workdir config.yaml | cut -d ' ' -f 2 | tr -d "'")" # oh no, very bad
-mkdir -p "./$workdir"
-cp -v "lsf.yaml" "./$workdir"
-
 bsub \
   -N \
   -R 'rusage[mem=2000]' \
